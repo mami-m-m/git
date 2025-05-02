7fe40a7bfd7853b88768ac9aae9aeb9700795f3e
Mami Morinaga <mami.morinaga@avanade.com>
2025-05-02 16:17:31 +0900

    104

--- git/Form2.cs
+++ git/Form2.cs
@@ -101,7 +101,7 @@ namespace git
 
         private void panel1_Paint(object sender, PaintEventArgs e)
         {
-            Console.WriteLine("panel1_Paint");
+            Console.WriteLine("panel1");
         }
 
         private void FSnakeGame_KeyDown(object sender, KeyEventArgs e)
